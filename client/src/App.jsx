7154c57994fd046a1bb69c57c56a1493a2448667
prathamesh-pichkate <prathameshpichkate@gmail.com>
2024-11-05 14:59:29 +0530

    Set up the pages inn client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import About from "./pages/About";
 export default function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ note.md
@@ -0,0 +1,10 @@
+### 1. Set Up :
+
+Client Setup :
+1.Install the packages:
+
+```
+npm i vite@latest
+npm i react-rouer-dom
+npm i react-router-dom@latest
+```
