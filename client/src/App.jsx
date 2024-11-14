9dab7559b7e1bb0edc47061308ac262f357c1786
prathamesh-pichkate <prathameshpichkate@gmail.com>
2024-11-14 10:26:13 +0530

    Added the private Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,13 +6,16 @@ import Signup from "./pages/Signup";
 import About from "./pages/About";
 import Header from "./components/Header";
 import FooterComponent from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 export default function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<Signin />} />
         <Route path="/sign-up" element={<Signup />} />
