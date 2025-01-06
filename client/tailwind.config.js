df73291fd88a2b52b165918d724838bea2302b7b
prathamesh-pichkate <prathameshpichkate@gmail.com>
2025-01-06 12:21:45 +0530

    Updated the tailwind file

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,11 @@
-const flowbite = require("flowbite-react/tailwind");
-/** @type {import('tailwindcss').Config} */
+import flowbite from "flowbite-react/tailwind";
+import tailwindScrollbar from "tailwind-scrollbar";
 
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", ...flowbite.content],
   theme: {
     extend: {},
   },
-  plugins: [flowbite.plugin(), require("tailwind-scrollbar")],
+  plugins: [flowbite.plugin, tailwindScrollbar],
 };
